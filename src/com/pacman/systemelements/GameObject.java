507652546fecde89a34950c4ae92ec714fca0fab
Lucas Stapf <62681897+LucasStapf@users.noreply.github.com>
2021-11-16 17:23:24 -0300

    Update GameObject.java

--- src/com/pacman/systemelements/GameObject.java
+++ src/com/pacman/systemelements/GameObject.java
@@ -1,8 +1,10 @@
 package com.pacman.systemelements;
 
+import com.pacman.engine.Time;
+
 public abstract class GameObject extends SceneElement {
 
-    private HitBox hitBox = new HitBox();
+    private HitBox hitBox = new HitBox(getDimension(), getPosition());
     private GameObject collider;
     private Velocity velocity;
 
@@ -37,5 +39,48 @@ public abstract class GameObject extends SceneElement {
 
     public void setRigidBody(boolean rigidBody) {
         this.rigidBody = rigidBody;
+    }
+
+    public void translate() {
+
+        float x = getPosition().getX();
+        float y = getPosition().getY();
+
+        switch (velocity.getDirection()) {
+
+            case UP:
+                y += velocity.getModulus() * Time.getDeltaTimeSEC();
+                getPosition().setY(y);
+                break;
+
+            case DOWN:
+                y -= velocity.getModulus() * Time.getDeltaTimeSEC();
+                getPosition().setY(y);
+                break;
+
+            case LEFT:
+                x -= velocity.getModulus() * Time.getDeltaTimeSEC();
+                getPosition().setX(x);
+                break;
+
+            case RIGHT:
+                x += velocity.getModulus() * Time.getDeltaTimeSEC();
+                getPosition().setX(x);
+                break;
+        }
+
+        getHitBox().setPosition(getPosition());
+    }
+
+    public boolean isCenteredOnFloor(Floor floor) {
+        float deltaX = Math.abs(getPosition().getX() - floor.getPosition().getX());
+        float deltaY = Math.abs(getPosition().getY() - floor.getPosition().getY());
+        return (deltaX < 0.10f && deltaY < 0.10f);
+    }
+
+    @Override
+    public void setPosition(Position position) throws NullPointerException {
+        super.setPosition(position);
+        getHitBox().setPosition(getPosition());
     }
 }
