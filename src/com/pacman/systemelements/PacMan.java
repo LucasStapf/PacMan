8018f222495919deb559912c463bf2ab66589f40
Lucas Stapf <62681897+LucasStapf@users.noreply.github.com>
2021-11-23 08:19:45 -0300

    Create PacMan.java

---
+++ src/com/pacman/systemelements/PacMan.java
@@ -0,0 +1,25 @@
+package com.pacman.systemelements;
+
+public class PacMan extends GameObject {
+
+    public PacMan() {
+        setLayer(2);
+        setRigidBody(true);
+    }
+
+    public PacMan(Position position) {
+        setLayer(2);
+        setRigidBody(true);
+        setPosition(position);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void onCollision() {
+
+    }
+}
