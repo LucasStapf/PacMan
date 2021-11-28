9af88c45b2a2360c730341604ac02d3d842ab3cc
Lucas Stapf <62681897+LucasStapf@users.noreply.github.com>
2021-11-28 10:00:56 -0300

    Update JavaDoc

--- src/com/pacman/systemelements/Floor.java
+++ src/com/pacman/systemelements/Floor.java
@@ -2,19 +2,43 @@ package com.pacman.systemelements;
 
 import com.pacman.engine.Vertex;
 
+/**
+ * Classe que representa o chão do tabuleiro.
+ */
 public class Floor extends SceneElement {
 
+    /**
+     * Vértice associado ao Floor.
+     */
     private Vertex<Floor> vertex = new Vertex<>(this);
 
+    /**
+     * Atributo temporário para marcar o caminho.
+     */
     public boolean highlighted = false;
 
+    /**
+     * Construtor padrão.
+     * Por padrão, a camada (layer) do Floor é zero.
+     * @param position posição em que o Floor será criado.
+     */
     public Floor(Position position) {
         setLayer(0);
         setPosition(position);
     }
 
+    /**
+     * Retorna o vértice associado ao Floor.
+     * @return vertex
+     */
     public Vertex<Floor> getVertex() {
         return vertex;
+    }
+
+    @Override
+    public void print() {
+        if (highlighted) System.out.print("o");
+        else System.out.print(" ");
     }
 
     @Override
@@ -22,11 +46,5 @@ public class Floor extends SceneElement {
         return "Floor{" +
                 "Position=" + getPosition() +
                 '}';
-    }
-
-    @Override
-    public void print() {
-        if (highlighted) System.out.print("o");
-        else System.out.print(" ");
     }
 }
