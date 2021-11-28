e355cc12a466cd9d07e5818419bad3756f30d693
Lucas Stapf <62681897+LucasStapf@users.noreply.github.com>
2021-11-28 07:59:28 -0300

    Update Dimension JavaDoc

--- src/com/pacman/systemelements/Dimension.java
+++ src/com/pacman/systemelements/Dimension.java
@@ -1,46 +1,56 @@
 package com.pacman.systemelements;
 
+/**
+ * Classe que representa as dimensões de um SceneElement.
+ */
 public final class Dimension {
 
+    /**
+     * Largura do SceneElement.
+     */
     private final float width;
+
+    /**
+     * Altura do SceneElement.
+     */
     private final float height;
 
+    /**
+     * Construtor padrão. Os valores de altura e largura serão ambos inicializados com0 0.0f.
+     */
     public Dimension() {
         this.width = 0.0f;
         this.height = 0.0f;
     }
 
+    /**
+     * Construtor no qual os valores das dimensões são passadas.
+     * @param width largura do SceneElement
+     * @param height altura do SceneElement
+     * @throws IllegalArgumentException caso algum dos parâmetros passados seja menor que zero.
+     */
     public Dimension(float width, float height) throws IllegalArgumentException {
         if (width < 0) throw new IllegalArgumentException("Width can not be a negative number");
         if (height < 0) throw new IllegalArgumentException("Height can not be a negative number");
         this.width = width;
         this.height = height;
-//        setWidth(width);
-//        setHeight(height);
     }
 
+    /**
+     * Retorna a largura do SceneElement.
+     * @return width
+     */
     public float getWidth() {
         return width;
     }
 
+    /**
+     * Retorna a altura do SceneElement.
+     * @return height
+     */
     public float getHeight() {
         return height;
     }
-
-//    public void setWidth(float width) throws IllegalArgumentException {
-//        if (width < 0) throw new IllegalArgumentException("Width can not be a negative number");
-//        this.width = width;
-//    }
-//
-//    public void setHeight(float height) throws IllegalArgumentException {
-//        if (height < 0) throw new IllegalArgumentException("Height can not be a negative number");
-//        this.height = height;
-//    }
-
-//    public void updateDimension(float width, float height) throws IllegalArgumentException {
-//        setWidth(width);
-//        setHeight(height);
-//    }
 
     @Override
     public String toString() {
