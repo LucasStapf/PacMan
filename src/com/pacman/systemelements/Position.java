dbe7f5e78132d0758b5164f9409d70aa3cf2ed5b
Lucas Stapf <62681897+LucasStapf@users.noreply.github.com>
2021-11-28 08:08:53 -0300

    Update Position JavaDoc

--- src/com/pacman/systemelements/Position.java
+++ src/com/pacman/systemelements/Position.java
@@ -1,45 +1,54 @@
 package com.pacman.systemelements;
 
+/**
+ * Classe que representa a posição do SceneElement no tabuleiro.
+ */
 public final class Position {
 
-    private float x;
-    private float y;
+    /**
+     * Representa a abscissa do SceneElement.
+     */
+    private final float x;
 
+    /**
+     * Representa a ordenada do SceneElement.
+     */
+    private final float y;
+
+    /**
+     * Construtor padrão. Tanto a abscissa quanto ordenadas serão inicializados como zero.
+     */
     public Position() {
         this.x = 0.0f;
         this.y = 0.0f;
     }
 
+    /**
+     * Construtor com os parâmetros da abscissa e ordenadas sendo passados.
+     * @param x abscissa do SceneElement.
+     * @param y ordenada do SceneElement.
+     */
     public Position(float x, float y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Retorna a abscissa do SceneElement.
+     * @return
+     */
     public float getX() {
         return x;
     }
 
+    /**
+     * Retorna a ordenada do SceneElement.
+     * @return
+     */
     public float getY() {
         return y;
     }
 
-    public void setX(float x) {
-        this.x = x;
-    }
-
-    public void setY(float y) {
-        this.y = y;
-    }
-
-    public void updatePosition(float x, float y) {
-        setX(x);
-        setY(y);
-    }
-
-    public void copy(Position position) {
-        this.x = position.x;
-        this.y = position.y;
-    }
 
     @Override
     public String toString() {
