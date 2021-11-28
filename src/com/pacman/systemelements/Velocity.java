9931d8c2a5d81c70eab6fe03a952c999dcb5fc6f
Lucas Stapf <62681897+LucasStapf@users.noreply.github.com>
2021-11-28 09:05:33 -0300

    Update JavaDoc

--- src/com/pacman/systemelements/Velocity.java
+++ src/com/pacman/systemelements/Velocity.java
@@ -1,7 +1,13 @@
 package com.pacman.systemelements;
 
+/**
+ * Classe que representa a velocidade de um DynamicGameObject.
+ */
 public final class Velocity {
 
+    /**
+     * Representa a direção da velocidade.
+     */
     public enum Direction {
         UP,
         DOWN,
@@ -10,55 +16,127 @@ public final class Velocity {
         NONE;
     }
 
+    /**
+     * Indica o módulo da velocidade.
+     */
     private float modulus;
+
+    /**
+     * Guarda a direção da velocidade.
+     */
     private Direction direction;
 
+    /**
+     * Construtor padrão. Velocidade possui módulo zero e não tem direção definida.
+     */
     public Velocity() {
         this.modulus = 0.0f;
         this.direction = Direction.NONE;
     }
 
+    /**
+     * Construtor no qual são passadas os parâmetros da velocidade.
+     * @param modulus módulo da velocidade. Não pode ser negativo.
+     * @param direction direção da velocidade.
+     * @throws IllegalArgumentException caso a valor do módulo passado seja menor que zero.
+     */
     public Velocity(float modulus, Direction direction) throws IllegalArgumentException {
         if (modulus < 0) throw new IllegalArgumentException("Modulos must be a positive number");
         this.modulus = modulus;
         this.direction = direction;
     }
 
+    /**
+     * Retorna o módulo da velocidade.
+     * @return modulus
+     */
     public float getModulus() {
         return modulus;
     }
 
+    /**
+     * Retorna a direção da velocidade.
+     * @return direction
+     */
     public Direction getDirection() {
         return direction;
     }
 
+    /**
+     * Altera o valor do módulo da velocidade.
+     * @param modulus novo valor do módulo. Deve ser não-negativo.
+     * @throws IllegalArgumentException caso o valor passado seja negativo.
+     */
     public void setModulus(float modulus) throws IllegalArgumentException {
         if (modulus < 0) throw new IllegalArgumentException(); // msg
         this.modulus = modulus;
     }
 
+    /**
+     * Altera a direção da velocidade.
+     * @param direction nova direção da velocidade.
+     */
     public void setDirection(Direction direction) {
         this.direction = direction;
     }
 
+    /**
+     * Altera tanto módulo quanto direção da velocidade.
+     * @param modulus novo valor do módulo. Deve ser não-negativo.
+     * @param direction nova direção.
+     * @throws IllegalArgumentException caso o valor do módulo passado seja negativo.
+     */
     public void updateVelocity(float modulus, Direction direction) throws IllegalArgumentException {
         setModulus(modulus);
         setDirection(direction);
     }
 
-    public static boolean isHorizontal(Velocity velocity) {
+    /**
+     * Verifica se a direção é horizontal ou não.
+     * Direções horizontais: RIGHT e LEFT.
+     * @param velocity velocidade a ser analisada. Não pode ser null.
+     * @return true caso seja horizontal, false caso contrário.
+     * @throws NullPointerException caso a velocidade passada seja null.
+     */
+    public static boolean isHorizontal(Velocity velocity) throws NullPointerException {
+        if (velocity == null) throw new NullPointerException();
         return (velocity.direction.equals(Direction.RIGHT) || velocity.direction.equals(Direction.LEFT));
     }
 
-    public static boolean isVertical(Velocity velocity) {
+    /**
+     * Verifica se a direção é vertical ou não.
+     * Direções verticais: UP e DOWN.
+     * @param velocity velocidade a ser analisada. Não pode ser null.
+     * @return true caso seja vertical, false caso contrário.
+     * @throws NullPointerException caso a velocidade passada seja null.
+     */
+    public static boolean isVertical(Velocity velocity) throws NullPointerException {
+        if (velocity == null) throw new NullPointerException();
         return (velocity.direction.equals(Direction.UP) || velocity.direction.equals(Direction.DOWN));
     }
 
-    public static boolean isSameDirection(Velocity v1, Velocity v2) {
+    /**
+     * Verifica se as duas velocidades pssuem a mesma direção.
+     * @param v1 primeira velocidade. Não pode ser null.
+     * @param v2 segunda velocidade. Não pode ser null.
+     * @return true caso possuam mesma direção (ou uma não possuir direção definida), false caso contrário.
+     * @throws NullPointerException caso uma ou mais velocidades sejam null.
+     */
+    public static boolean isSameDirection(Velocity v1, Velocity v2) throws NullPointerException {
+        if (v1 == null || v2 == null) throw new NullPointerException();
         return v1.direction.equals(v2.direction);
     }
 
-    public static boolean isOppositeDirection(Velocity v1, Velocity v2) {
+    /**
+     * Verifica se as duas velocidade possuem direção oposta.
+     * @param v1 primeira velocidade. Não pode ser null.
+     * @param v2 segunda velocidade. Não pode ser null.
+     * @return true caso possuam direção oposta (ou um não possuir direção definida), false caso contrário.
+     * @throws NullPointerException caso uma ou mais velocidades sejam null.
+     */
+    public static boolean isOppositeDirection(Velocity v1, Velocity v2) throws NullPointerException {
+
+        if (v1 == null || v2 == null) throw new NullPointerException();
 
         if (v1.direction.equals(Direction.UP)) {
             if (v2.direction.equals(Direction.DOWN)) return true;
@@ -83,18 +161,27 @@ public final class Velocity {
         return true;
     }
 
-    public static boolean isPerpendicularDirection(Velocity v1, Velocity v2) {
+    /**
+     * Verifica se duas velocidade são perpendiculares ou não.
+     * @param v1 primeira velocidade. Não pode ser null.
+     * @param v2 segunda velocidade. Não pode ser null.
+     * @return true caso sejam perpediculares, false caso contrário.
+     * @throws NullPointerException
+     */
+    public static boolean isPerpendicularDirection(Velocity v1, Velocity v2) throws NullPointerException {
+        if (v1 == null || v2 == null) throw new NullPointerException();
         if (v1.direction.equals(Direction.NONE) || v2.direction.equals(Direction.NONE)) return false;
         else if (isSameDirection(v1, v2)) return false;
         else if (isOppositeDirection(v1, v2)) return false;
         else return true;
     }
 
-    public static int sign(Velocity v) {
-        if (v.direction.equals(Direction.UP) || v.direction.equals(Direction.RIGHT)) return 1;
-        else if (v.direction.equals(Direction.DOWN) || v.direction.equals(Direction.LEFT)) return -1;
-        else return 0;
-    }
+//    public static int sign(Velocity velocity) throws NullPointerException {
+//        if (velocity == null) throw new NullPointerException();
+//        if (velocity.direction.equals(Direction.UP) || velocity.direction.equals(Direction.RIGHT)) return 1;
+//        else if (velocity.direction.equals(Direction.DOWN) || velocity.direction.equals(Direction.LEFT)) return -1;
+//        else return 0;
+//    }
 
     @Override
     public String toString() {
