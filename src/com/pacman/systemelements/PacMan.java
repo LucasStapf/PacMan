f3d7f4cd96e9de51997b0d30e0ec7e109421fd6d
Lucas Stapf <62681897+LucasStapf@users.noreply.github.com>
2021-11-28 10:26:46 -0300

    Update JavaDoc

--- src/com/pacman/systemelements/PacMan.java
+++ src/com/pacman/systemelements/PacMan.java
@@ -1,29 +1,52 @@
 package com.pacman.systemelements;
 
+import com.pacman.engine.GameManager;
+
+/**
+ * Classe que representa o Pac-Man do jogo.
+ */
 public class PacMan extends DynamicGameObject {
 
+    /**
+     * Atributo que registra a quantidade de vida que o PacMan possui.
+     */
     private int life;
+
+    /**
+     * Atributo que guarda a quantidade de pontos obtidos pelo PacMan.
+     */
     private int totalPoints;
 
-    public PacMan() {
-        setLayer(2);
-        setRigidBody(true);
-    }
-
+    /**
+     * Construtor padrão.
+     * Por padrão, a camada do PacMan é 3 (layer = 3) e é um corpo rígido (rigidBody = true).
+     * @param position
+     */
     public PacMan(Position position) {
-        setLayer(2);
+        setLayer(3);
         setRigidBody(true);
         setPosition(position);
     }
 
     @Override
     public void update() {
-
+        translate(1);
     }
 
     @Override
     public void onCollision() {
-        System.out.println("Collision! Collider is: " + getCollider());
+
+        if (getCollider() instanceof Ghost) {
+            System.out.println("Collision with Ghost!");
+            GameManager.end();
+        }
+
+        // temp
+        if (getVelocity().getDirection() == Velocity.Direction.LEFT) {
+            getVelocity().setDirection(Velocity.Direction.RIGHT);
+        } else if (getVelocity().getDirection() == Velocity.Direction.RIGHT) {
+            getVelocity().setDirection(Velocity.Direction.LEFT);
+        }
     }
 
     @Override
