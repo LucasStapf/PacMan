e0a8a85a80c90e0e01b19d840e133173ffd840dc
Lucas Stapf <62681897+LucasStapf@users.noreply.github.com>
2021-11-28 09:55:45 -0300

    Update JavaDoc

--- src/com/pacman/systemelements/EnergyPill.java
+++ src/com/pacman/systemelements/EnergyPill.java
@@ -1,7 +1,14 @@
 package com.pacman.systemelements;
 
+/**
+ * Classe que representa uma pílula de energia do jogo.
+ */
 public class EnergyPill extends GameObject {
 
+    /**
+     * Construtor padrão.
+     * @param position posição em que o EnergyPill será criado.
+     */
     public EnergyPill(Position position) {
         setLayer(2);
         setRigidBody(false);
