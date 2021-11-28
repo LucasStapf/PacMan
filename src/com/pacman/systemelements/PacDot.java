5d9e10d585d2de245038f801e655842865391c7e
Lucas Stapf <62681897+LucasStapf@users.noreply.github.com>
2021-11-28 10:19:07 -0300

    Update JavaDoc

--- src/com/pacman/systemelements/PacDot.java
+++ src/com/pacman/systemelements/PacDot.java
@@ -1,7 +1,15 @@
 package com.pacman.systemelements;
 
+/**
+ * Classe que representa um pacdot do jogo.
+ */
 public class PacDot extends GameObject {
 
+    /**
+     * Construtor padrão.
+     * Por padrão, a camada do PacDot é 2 (layer = 2) e não é um corpo rigido (rigidBody = false).
+     * @param position
+     */
     public PacDot (Position position) {
         setLayer(2);
         setRigidBody(false);
