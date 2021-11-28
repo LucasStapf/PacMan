b0c8e49ab9bad30df785ea2734feb9d491c90df5
Lucas Stapf <62681897+LucasStapf@users.noreply.github.com>
2021-11-28 10:43:21 -0300

    Update JavaDoc

--- src/com/pacman/engine/Vertex.java
+++ src/com/pacman/engine/Vertex.java
@@ -1,13 +1,28 @@
 package com.pacman.engine;
 
+/**
+ * Classe que representa um vértice de um grafo.
+ * @param <T> Tipo de dado que será relacionado ao vértice.
+ */
 public class Vertex<T> {
 
+    /**
+     * Tipo de dado não definido que será relacionado ao vértice.
+     */
     private T t;
 
+    /**
+     * Construtor padrão.
+     * @param t Tipo de dado que será relacionado ao vértice.
+     */
     public Vertex(T t) {
         this.t = t;
     }
 
+    /**
+     * Método que retorna o dado relaciona ao vértice.
+     * @return t
+     */
     public T getT() {
         return t;
     }
