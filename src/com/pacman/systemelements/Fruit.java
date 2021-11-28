d22dcecaa90743911cbcb5f8669a0cba3167c38b
Lucas Stapf <62681897+LucasStapf@users.noreply.github.com>
2021-11-28 10:03:11 -0300

    Update JavaDoc

--- src/com/pacman/systemelements/Fruit.java
+++ src/com/pacman/systemelements/Fruit.java
@@ -1,7 +1,14 @@
 package com.pacman.systemelements;
 
+/**
+ * Classe que representa uma fruta do jogo.
+ */
 public class Fruit extends GameObject {
 
+    /**
+     * Construtor padrão.
+     * @param position posição em que a Fruit será criado.
+     */
     public Fruit(Position position) {
         setLayer(2);
         setRigidBody(false);
