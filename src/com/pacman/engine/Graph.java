064fe32e014e0e616a1ba487790df71abc198cc4
Lucas Stapf <62681897+LucasStapf@users.noreply.github.com>
2021-11-28 14:48:49 -0300

    Update JavaDoc

--- src/com/pacman/engine/Graph.java
+++ src/com/pacman/engine/Graph.java
@@ -4,33 +4,48 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
+/**
+ * Classe que representa um Grafo.
+ * É utilizado para encontrar o menor caminho entre dois pontos do tabuleiro.
+ */
 public class Graph {
 
+    /**
+     * Atributo que guarda a lista de vértices adjacentes de um determinado vértice.
+     */
     private Map<Vertex, LinkedList<Vertex>> adjVertices;
 
+    /**
+     * Construtor padrão.
+     */
     public Graph() {
         adjVertices = new HashMap<Vertex, LinkedList<Vertex>>();
     }
 
-    public void addVertex(Vertex vertex) {
-        adjVertices.put(vertex, new LinkedList<Vertex>());
-    }
-
+    /**
+     * Método que retorna a lista de vértices adjacentes.
+     * @return adjVertices
+     */
     public Map<Vertex, LinkedList<Vertex>> getAdjVertices() {
         return adjVertices;
     }
 
-    public void removeVertex(Vertex vertex) {
-        adjVertices.remove(vertex);
+    /**
+     * Método que adiciona um vértice ao grafo.
+     * @param vertex vértice a ser adicionado.
+     */
+    public void addVertex(Vertex vertex) {
+        adjVertices.put(vertex, new LinkedList<Vertex>());
     }
-
+    
+    /**
+     *
+     * Método que cria uma aresta entre 2 vértices.
+     * @param v1 Vértice 1.
+     * @param v2 Vértice 2.
+     */
     public void addEdge(Vertex v1, Vertex v2) {
         adjVertices.get(v1).add(v2);
         adjVertices.get(v2).add(v1);
-    }
-
-    public void removeEdge(Vertex v1, Vertex v2) {
-        adjVertices.get(v1).remove(v2);
-        adjVertices.get(v2).remove(v1);
     }
 }
