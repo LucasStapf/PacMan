956b4a08b84509fbfc157dcb4433d01b03d96222
Lucas Stapf <62681897+LucasStapf@users.noreply.github.com>
2022-01-08 18:59:07 -0300

    Alterados métodos da interface.

--- src/com/pacman/graphicinterface/GameObjectController.java
+++ src/com/pacman/graphicinterface/GameObjectController.java
@@ -1,15 +1,54 @@
 package com.pacman.graphicinterface;
 
+import com.pacman.graphicinterface.components.javafx.SceneElementGraphic;
 import com.pacman.systemelements.GameObject;
 import javafx.animation.KeyFrame;
 import javafx.scene.layout.Region;
 
 public interface GameObjectController {
 
-    public abstract void setGameObject(GameObject gameObject) throws IllegalArgumentException;
-    public abstract Region getGameObjectID();
-    public abstract void updateGameObjectID();
-    public abstract KeyFrame getKeyFrame();
-    public abstract void destroy();
+    /**
+     * Retorna o {@link GameObject} controlado.
+     * @return o {@link GameObject} controlado.
+     */
+    GameObject getGameObject();
+
+    /**
+     * Altera o {@link GameObject} controlado.
+     * @param gameObject novo {@link GameObject}.
+     * @throws IllegalArgumentException caso a classe do objeto não seja a requerida.
+     */
+    void setGameObject(GameObject gameObject) throws IllegalArgumentException;
+
+    /**
+     * Retorna a representação gráfica do {@link GameObject} controlado.
+     * @return a representação gráfica do {@link GameObject} controlado.
+     */
+    SceneElementGraphic getSceneElementGraphic();
+
+    /**
+     * Altera a representação gráfica do {@link GameObject} controlado.
+     * @param sceneElementGraphic nova representação gráfica.
+     */
+    void setSceneElementGraphic(SceneElementGraphic sceneElementGraphic);
+
+    /**
+     * Retorna o KeyFrame responsável pela translação do {@link GameObject} em questão.
+     * @return o KeyFrame responsável pela translação do {@link GameObject} em questão.
+     * @param x abscissa do ponto de chegada.
+     * @param y ordenada do ponto de chegada.
+     */
+    KeyFrame getTranslationKeyFrame(double x, double y);
+
+    /**
+     * Retorna o KeyFrame responsável pela animação do {@link GameObject} e questão.
+     * @return o KeyFrame responsável pela animação do {@link GameObject} e questão.
+     */
+    KeyFrame getAnimationKeyFrame();
+
+    /**
+     * Destrói tanto o {@link GameObject} controlado quanto sua representação gráfica.
+     */
+    void destroy();
 
 }
