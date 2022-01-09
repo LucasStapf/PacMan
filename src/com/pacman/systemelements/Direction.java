c394678f2138abbb79611825f53b0e0e1cb222f7
Lucas Stapf <62681897+LucasStapf@users.noreply.github.com>
2022-01-09 03:22:11 -0300

    Commit inicial.

---
+++ src/com/pacman/systemelements/Direction.java
@@ -0,0 +1,32 @@
+package com.pacman.systemelements;
+
+/**
+ * Representa uma direção no plano XY.
+ */
+public enum Direction {
+
+    /**
+     * Sentido positivo do eixo das ordenadas.
+     */
+    UP,
+
+    /**
+     * Sentido negativo do eixo das ordenadas.
+     */
+    DOWN,
+
+    /**
+     * Sentido positivo do eixo das abscissas.
+     */
+    LEFT,
+
+    /**
+     * Sentido negativo do eixo das abscissas.
+     */
+    RIGHT,
+
+    /**
+     * Sem direção definida.
+     */
+    NONE
+}
