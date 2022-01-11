3ab790c2a07ed4e67fc6ce411fd5f3e76542fc2a
Lucas Stapf <62681897+LucasStapf@users.noreply.github.com>
2022-01-11 14:45:17 -0300

    Commit inicial.

---
+++ src/com/pacman/graphicinterface/components/controllers/WallController.java
@@ -0,0 +1,63 @@
+package com.pacman.graphicinterface.components.controllers;
+
+import com.pacman.graphicinterface.components.javafx.SceneElementGraphic;
+import com.pacman.graphicinterface.components.javafx.WallGraphic;
+import com.pacman.systemelements.GameObject;
+import com.pacman.systemelements.Wall;
+import javafx.animation.KeyFrame;
+
+public class WallController implements GameObjectController {
+
+    /**
+     * Atributo responsável por guardar o objeto {@link Wall}.
+     */
+    private Wall wall;
+
+    @Override
+    public GameObject getGameObject() {
+        return wall;
+    }
+
+    @Override
+    public void setGameObject(GameObject gameObject) throws IllegalArgumentException {
+        if (!(gameObject instanceof Wall)) throw new IllegalArgumentException("GameObject must be a Wall!");
+        else wall = (Wall) gameObject;
+    }
+
+    /**
+     * Atributo responsável por guardar a representação gráfica do {@link Wall}.
+     */
+    private WallGraphic wallGraphic;
+
+    @Override
+    public SceneElementGraphic getSceneElementGraphic() {
+        return wallGraphic;
+    }
+
+    @Override
+    public void setSceneElementGraphic(SceneElementGraphic sceneElementGraphic) throws IllegalArgumentException {
+        if (!(sceneElementGraphic instanceof WallGraphic)) {
+            throw new IllegalArgumentException("The SceneElementGraphic must be a WallGraphic object.");
+        } else wallGraphic = (WallGraphic) sceneElementGraphic;
+    }
+
+    @Override
+    public KeyFrame getTranslationKeyFrame() {
+        return null;
+    }
+
+    @Override
+    public KeyFrame getAnimationKeyFrame() {
+        return null;
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void destroy() {
+//        SystemGame.getScreenManager().getBoardPane().getChildren().remove(wallID);
+    }
+}
