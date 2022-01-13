7b24e64f1e517c9a393e368e77e6d2602ed594a4
Lucas Stapf <62681897+LucasStapf@users.noreply.github.com>
2022-01-13 16:12:05 -0300

    Adição de um construtor padrão.

--- src/com/pacman/systemelements/Place.java
+++ src/com/pacman/systemelements/Place.java
@@ -2,6 +2,12 @@ package com.pacman.systemelements;
 
 public final class Place extends GameObject {
 
+    public Place(Position position) {
+        setPosition(position);
+        setOldPosition(position);
+        setRigidBody(false);
+    }
+
     @Override
     public void update() {
 
