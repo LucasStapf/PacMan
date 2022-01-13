f4974f57afd1b77de45540d08cd46e4925f37c18
Lucas Stapf <62681897+LucasStapf@users.noreply.github.com>
2022-01-13 07:57:04 -0300

    Adição de atributos que guardam as pontuações do jogo.

--- src/com/pacman/engine/ScoreManager.java
+++ src/com/pacman/engine/ScoreManager.java
@@ -1,10 +1,48 @@
 package com.pacman.engine;
 
-import com.pacman.systemelements.Ghost;
-import com.pacman.systemelements.PacDot;
-import com.pacman.systemelements.PacMan;
-
 public class ScoreManager {
+
+    /**
+     * Pontuação máxima atingida no jogo.
+     */
+    private int highScore;
+
+    /**
+     * Retorna a pontuação máxima atingida no jogo.
+     * @return a pontuação máxima atingida no jogo.
+     */
+    public int highScore() {
+        return highScore;
+    }
+
+    /**
+     * Altera a pontuação máxima atingida no jogo.
+     * @param highScore nova pontuação.
+     */
+    public void setHighScore(int highScore) {
+        this.highScore = highScore;
+    }
+
+    /**
+     * Pontuação atual.
+     */
+    private int gameScore;
+
+    /**
+     * Retorna a pontuação atual no jogo.
+     * @return a pontuação atual no jogo.
+     */
+    public int gameScore() {
+        return gameScore;
+    }
+
+    /**
+     * Altera a pontuação atual no jogo.
+     * @param gameScore nova pontuação.
+     */
+    public void setGameScore(int gameScore) {
+        this.gameScore = gameScore;
+    }
 
     private static final int bonus = 1;
     private static int bonusPacDot = 1;
