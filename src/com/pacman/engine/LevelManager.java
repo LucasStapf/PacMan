487786478916e085bf790630c86ae02b9bee2d58
Lucas Stapf <62681897+LucasStapf@users.noreply.github.com>
2022-01-13 11:42:56 -0300

    Adição de métodos para lidar com o efeito da pílula de energia.

--- src/com/pacman/engine/LevelManager.java
+++ src/com/pacman/engine/LevelManager.java
@@ -1,5 +1,9 @@
 package com.pacman.engine;
 
+import com.pacman.graphicinterface.components.javafx.GhostGraphic;
+import com.pacman.systemelements.Ghost;
+import javafx.scene.paint.Color;
+
 public class LevelManager {
 
     /**
@@ -45,4 +49,60 @@ public class LevelManager {
     }
 
 
+    private boolean effectEnergyPill = false;
+
+    public final static long deltaTimeEffect = 10000;
+    private long timeStarted;
+
+    public void startEffectEnergyPill() {
+
+        effectEnergyPill = true;
+        timeStarted = System.currentTimeMillis();
+
+        Color color = Color.CADETBLUE;
+
+        ((Ghost) GameSystem.gameObjectManager.blinky().getGameObject()).setVulnerable(true);
+        ((Ghost) GameSystem.gameObjectManager.blinky().getGameObject()).setMovement(Ghost.Movement.RANDOM);
+        ((GhostGraphic) GameSystem.gameObjectManager.blinky().getSceneElementGraphic()).setBodyColor(color);
+
+        ((Ghost) GameSystem.gameObjectManager.pinky().getGameObject()).setVulnerable(true);
+        ((Ghost) GameSystem.gameObjectManager.pinky().getGameObject()).setMovement(Ghost.Movement.RANDOM);
+        ((GhostGraphic) GameSystem.gameObjectManager.pinky().getSceneElementGraphic()).setBodyColor(color);
+
+        ((Ghost) GameSystem.gameObjectManager.inky().getGameObject()).setVulnerable(true);
+        ((GhostGraphic) GameSystem.gameObjectManager.inky().getSceneElementGraphic()).setBodyColor(color);
+
+        ((Ghost) GameSystem.gameObjectManager.clyde().getGameObject()).setVulnerable(true);
+        ((GhostGraphic) GameSystem.gameObjectManager.clyde().getSceneElementGraphic()).setBodyColor(color);
+    }
+
+    public void checkEffectEnergyPill() {
+
+        if (effectEnergyPill && System.currentTimeMillis() - timeStarted > deltaTimeEffect) {
+
+            effectEnergyPill = false;
+
+            ((Ghost) GameSystem.gameObjectManager.blinky().getGameObject()).setVulnerable(false);
+            ((Ghost) GameSystem.gameObjectManager.blinky().getGameObject()).setMovement(Ghost.Movement.FOLLOW_TARGET);
+            ((GhostGraphic) GameSystem.gameObjectManager.blinky().getSceneElementGraphic()).setBodyColor(
+                    GameSystem.gameObjectManager.colorBlinky
+            );
+
+            ((Ghost) GameSystem.gameObjectManager.pinky().getGameObject()).setVulnerable(false);
+            ((Ghost) GameSystem.gameObjectManager.pinky().getGameObject()).setMovement(Ghost.Movement.FOLLOW_TARGET);
+            ((GhostGraphic) GameSystem.gameObjectManager.pinky().getSceneElementGraphic()).setBodyColor(
+                    GameSystem.gameObjectManager.colorPinky
+            );
+
+            ((Ghost) GameSystem.gameObjectManager.inky().getGameObject()).setVulnerable(false);
+            ((GhostGraphic) GameSystem.gameObjectManager.inky().getSceneElementGraphic()).setBodyColor(
+                    GameSystem.gameObjectManager.colorInky
+            );
+
+            ((Ghost) GameSystem.gameObjectManager.clyde().getGameObject()).setVulnerable(false);
+            ((GhostGraphic) GameSystem.gameObjectManager.clyde().getSceneElementGraphic()).setBodyColor(
+                    GameSystem.gameObjectManager.colorClyde
+            );
+        }
+    }
 }
