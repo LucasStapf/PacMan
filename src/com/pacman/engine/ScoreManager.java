660d94c3593d9cfd7eb0ba80fc7847141450a041
Lucas Stapf <62681897+LucasStapf@users.noreply.github.com>
2022-01-13 16:53:33 -0300

    Remoção de atributos e métodos.

--- src/com/pacman/engine/ScoreManager.java
+++ src/com/pacman/engine/ScoreManager.java
@@ -3,50 +3,6 @@ package com.pacman.engine;
 public class ScoreManager {
 
     /**
-     * Pontuação máxima atingida no jogo.
-     */
-    private int highScore = 0;
-
-    /**
-     * Retorna a pontuação máxima atingida no jogo.
-     * @return a pontuação máxima atingida no jogo.
-     */
-    public int highScore() {
-        return highScore;
-    }
-
-    /**
-     * Altera a pontuação máxima atingida no jogo.
-     * @param highScore nova pontuação.
-     */
-    public void setHighScore(int highScore) {
-        this.highScore = highScore;
-    }
-
-    /**
-     * Pontuação atual.
-     */
-    private int gameScore = 0;
-
-    /**
-     * Retorna a pontuação atual no jogo.
-     * @return a pontuação atual no jogo.
-     */
-    public int gameScore() {
-        return gameScore;
-    }
-
-    /**
-     * Altera a pontuação atual no jogo e atualiza o valor na tela.
-     * @param gameScore nova pontuação.
-     */
-    public void setGameScore(int gameScore) {
-        this.gameScore = gameScore;
-        GameSystem.screen.gameScoreBoard().setValue(gameScore);
-    }
-
-
-    /**
      * Bonus aplicado na pontuação padrão originada pelo Ghost.
      */
     public static int bonusScoreGhost = 1;
@@ -65,14 +21,5 @@ public class ScoreManager {
      * Quantidade de pontos dado pelo {@link com.pacman.systemelements.PacDot}
      */
     public final static int scoreFromPacDot = 10;
-
-    /**
-     * Método utilizado para adicionar pontos na pontuação atual.
-     * @param points quantidade a ser adicionada.
-     */
-    public void addGameScore(int points) {
-        int score = gameScore + points;
-        setGameScore(score);
-    }
 
 }
